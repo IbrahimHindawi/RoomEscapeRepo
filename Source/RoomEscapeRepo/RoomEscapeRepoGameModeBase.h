6f4f2c15be4cb0a0b854499324d3122ff4d3acd7
DESKTOP-9NUUM9N\m <ihindawi@gmail.com>
2018-01-07 15:55:29 +0200

    Initial commit
    Ignoring Stuff

---
+++ Source/RoomEscapeRepo/RoomEscapeRepoGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoomEscapeRepoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMESCAPEREPO_API ARoomEscapeRepoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
